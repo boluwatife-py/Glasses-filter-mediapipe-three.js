924f3089b637e66c3ffbe77ea643a3a155e30985
Boluwatife <boluwatife8081@gmail.com>
2025-03-05 16:11:41 +0100

    run build

--- src/js/camera_frame_provider.js
+++ src/js/camera_frame_provider.js
@@ -1,14 +1,20 @@
 import { Camera } from '@mediapipe/camera_utils';
 
 const overlay = document.getElementById("viewer-overlay");
+
 export class CameraFrameProvider {
   constructor(videoElement, onFrame) {
+    // Check if the camera is front-facing (user-facing)
+    const isFrontCamera = true; // You may need to detect this dynamically (see note below)
+    
     const camera = new Camera(videoElement, {
       onFrame: async () => {
-        onFrame(videoElement)
+        onFrame(videoElement);
       },
-      width: overlay.clientWidth,
-      height: overlay.clientHeight
+      width: overlay.clientWidth, // Use overlay width
+      height: overlay.clientHeight, // Use overlay height
+      facingMode: isFrontCamera ? 'user' : 'environment', // 'user' for front camera, 'environment' for back camera
+      mirror: isFrontCamera ? false : true, // Disable mirroring for front camera, enable for back camera
     });
     this.camera = camera;
   }
